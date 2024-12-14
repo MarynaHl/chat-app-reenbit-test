dd24f24d0aab1171a90536d004f8828f4fd42571
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-12-14 22:26:00 +0000

    upd

--- server/server.js
+++ server/server.js
@@ -1,27 +1,23 @@
 const express = require('express');
+const cors = require('cors'); 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// Load env vars from the root of the backend folder
-dotenv.config();  // За замовчуванням шукає .env в кореневій папці
+dotenv.config();
 
 const chatRoutes = require('./routes/chatRoutes');
-
 const app = express();
 
-// Body parser
-app.use(express.json());
+app.use(cors()); 
 
-// Mount routers
+app.use(express.json());
 app.use('/api/chats', chatRoutes);
 
-// DB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log('Error connecting to MongoDB:', err));
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
